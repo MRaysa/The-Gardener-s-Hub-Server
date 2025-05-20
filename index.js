49608602a23820740a94187c964c8db85ad32564
MRaysa <147266032+MRaysa@users.noreply.github.com>
2025-05-20 16:03:10 +0600

    try to fix for deploy

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const userCollection = client.db("allprojectsdb").collection("users");
+    // await client.connect();
+    const userCollection = client.db("gardensdb").collection("users");
     // start APIs backend
 
     // User related all database APIs
@@ -94,9 +94,9 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
