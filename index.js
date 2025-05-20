ade2338219c967f647e392917e5a145f5cc162cf
MRaysa <147266032+MRaysa@users.noreply.github.com>
2025-05-20 16:14:53 +0600

    try to fix for deploy

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function run() {
     app.get("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await coffeesCollection.findOne(query);
+      const result = await userCollection.findOne(query);
       res.send(result);
     });
 
